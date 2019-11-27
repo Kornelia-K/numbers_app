3fd21473c94b21d20ce6f31454460666df8bf9cc
unknown <kola.kornelia@gmail.com>
2019-11-27 17:56:51 +0100

    app

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,13 @@
 // build your app here
-export const app = () => console.log('hello world!');
+import axios from 'axios';
+
+const api_url = "http://numbersapi.com/random/trivia?json";
+
+export const app = () => {
+  axios.get(api_url)
+    .then(response => {
+        // console.log(response.data);
+        facts.innerText = response.data.text;
+      })
+    .catch(error => console.log(error));
+}
