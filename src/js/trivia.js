fe19f4b779d175e579ba89de0ef49ab937f2aaca
unknown <kola.kornelia@gmail.com>
2019-12-04 11:04:17 +0100

    date, year, math, tivia

---
+++ src/js/trivia.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const api_url = "http://numbersapi.com/random/trivia?json";
+
+export const trivia = () => {
+  axios.get(api_url)
+    .then(response => {
+        // console.log(response.data);
+        triviafacts.innerText = response.data.text;
+      })
+    .catch(error => console.log(error));
+   
+}
